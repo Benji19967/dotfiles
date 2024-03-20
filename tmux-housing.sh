e3fcc1977e25251e7b1fbc76513b39330c1378ea
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-03-20 15:48:11 +0100

    Update tmux script for creating workspace

--- tmux-housing.sh
+++ tmux-housing.sh
@@ -5,7 +5,20 @@ session="housing"
 tmux new-session -d -s $session
 
 window=0
-tmux rename-window -t $session:$window 'git'
+tmux rename-window -t $session:$window 'workspace'
+tmux send-keys -t $session:$window 'cd $CODE_REPO/housing' C-m
+tmux send-keys -t $session:$window 'source env/bin/activate' C-m
+tmux send-keys -t $session:$window 'vim .' C-m
+
+tmux split-window -t $session:$window -h
 tmux send-keys -t $session:$window 'cd $CODE_REPO/housing' C-m
 tmux send-keys -t $session:$window 'source env/bin/activate' C-m
 tmux send-keys -t $session:$window 'clear' C-m
+
+window=1
+tmux new-window -t $session:$window -n 'example-second-window'
+tmux send-keys -t $session:$window 'cd $CODE_REPO/housing' C-m
+tmux send-keys -t $session:$window 'source env/bin/activate' C-m
+tmux send-keys -t $session:$window 'vim .' C-m
+
+tmux select-window -t $session:workspace
