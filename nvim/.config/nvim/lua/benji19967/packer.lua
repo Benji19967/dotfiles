cf029bead2c7d71462d2784b29418ac4a7dc731f
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-10-18 13:16:17 +0200

    Add nvim recent projects

--- nvim/.config/nvim/lua/benji19967/packer.lua
+++ nvim/.config/nvim/lua/benji19967/packer.lua
@@ -16,14 +16,22 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-    use 'nvim-lua/plenary.nvim'
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-telescope/telescope-fzy-native.nvim'
     use 'windwp/nvim-autopairs'
     use 'numToStr/Comment.nvim' -- Easily comment stuff
     use 'nvim-tree/nvim-web-devicons'
     use 'nvim-tree/nvim-tree.lua'
-    use { "ellisonleao/glow.nvim" }
+    use 'ellisonleao/glow.nvim'
+
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'ahmedkhalf/project.nvim',
+            'nvim-telescope/telescope-project.nvim',
+            'nvim-telescope/telescope-fzy-native.nvim'
+        },
+    }
 
     -- cmp plugins
     use 'hrsh7th/nvim-cmp'
@@ -68,9 +76,6 @@ return require('packer').startup(function(use)
 
     -- Markdown preview in browser
     use "iamcco/markdown-preview.nvim"
-
-    -- Quickly navigate to projects
-    use "ahmedkhalf/project.nvim"
 
     -- Multi-cursor
     use 'mg979/vim-visual-multi'
