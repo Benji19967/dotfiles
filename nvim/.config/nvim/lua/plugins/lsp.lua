dd0ad897aa308b0abb9e9d662f997124eaf96bc5
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-11-20 12:05:34 +0100

    feat: add formatting via conform.nvim

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+
+      -- Autoformatting
+      "stevearc/conform.nvim",
+    },
+  },
+}
