43844f37c8c49b7fefd079528aea9b266d42fb23
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-11-20 21:58:00 +0100

    feat: setup lsp, move lsp keymaps

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,27 @@
 return {
   {
-    "neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig", -- collection of LSP servers for the Nvim LSP client
+  },
+  {
+    "mason-org/mason.nvim", -- package manager for LSP servers, formatters, linters
+    opts = {},
+  },
+  {
+    -- bridges nvim-lspconfig and mason
+    -- will automatically vim.lsp.enable() lsp servers installed via Mason
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      -- these will automatically be installed by Mason if they are not already installed
+      ensure_installed = {
+        "pyright",
+        "clangd",
+        "julials",
+        "lua_ls",
+      },
+    },
     dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-
-      -- Autoformatting
-      "stevearc/conform.nvim",
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
     },
   },
 }
