52c0df4816d2736db5084a518ba807b73db957e6
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-11-23 15:35:09 +0100

    feat: add new colorschemes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,48 @@
 return {
+  -- {
+  --   "loctvl842/monokai-pro.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("monokai-pro").setup({
+  --       filter = "light", -- this is the *light* version
+  --     })
+  --     vim.cmd("colorscheme monokai-pro")
+  --   end,
+  -- },
   {
-    "loctvl842/monokai-pro.nvim",
+    "Mofiqul/adwaita.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("monokai-pro").setup({
-        filter = "light", -- this is the *light* version
-      })
-      vim.cmd("colorscheme monokai-pro")
+      -- vim.g.adwaita_darker = true -- for darker version
+      -- vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+      -- vim.g.adwaita_transparent = true -- makes the background transparent
+      vim.cmd("colorscheme adwaita")
     end,
   },
+  -- {
+  --   "maxmx03/solarized.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   ---@type solarized.config
+  --   opts = {},
+  --   config = function(_, opts)
+  --     vim.o.termguicolors = true
+  --     vim.o.background = "light"
+  --     require("solarized").setup(opts)
+  --     vim.cmd.colorscheme("solarized")
+  --   end,
+  -- },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   priority = 1000,
+  --   config = true,
+  --   opts = ...,
+  --   config = function(_, opts)
+  --     vim.o.termguicolors = true
+  --     vim.o.background = "light"
+  --     vim.cmd.colorscheme("gruvbox")
+  --   end,
+  -- },
 }
