d3f0a13a520a688c6b8d634bc5f6ec166fc0d8a9
Benjamin Labrecque <labrecque.benji@gmail.com>
2026-06-25 16:32:25 +0200

    chore: fix treesitter and markdown -- neovim v12

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -2,7 +2,10 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
+    -- build = "cd app && yarn install",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,35 +5,38 @@
 
 -- Manual install of a parser:
 -- :TSInstall python
-
+--
 return {
   "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      branch = "main",
-      lazy = false,
-      build = ":TSUpdate",
-      ensure_installed = {
-        "python",
-        "lua",
-        "javascript",
-        "html",
-        "css",
-        "json",
-        "bash",
-        "regex",
-        "c",
-        "xml",
-        "java",
-      },
-      -- sync_install = true,
-      ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-      highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { "latex" }, -- list of language that will be disabled
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true, disable = { "yaml" } },
-    })
-  end,
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
+  main = "nvim-treesitter.config",
+  opts = {
+    ensure_installed = {
+      "python",
+      "lua",
+      "javascript",
+      "html",
+      "css",
+      "json",
+      "bash",
+      "regex",
+      "c",
+      "xml",
+      "java",
+      "markdown",
+      "markdown_inline",
+    },
+    ignore_install = { "phpdoc" },
+    highlight = {
+      enable = true,
+      disable = { "latex" },
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
+      disable = { "yaml" },
+    },
+  },
 }
